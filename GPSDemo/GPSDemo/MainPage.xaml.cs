0314c75622071bd956b6945f6cb846939dd8b944
Brett Sheleski <brett@sheleski.com>
2019-05-02 12:27:16 -0500

    initial commit

---
+++ GPSDemo/GPSDemo/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GPSDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            double latitude = 0, longitude = 0;
+
+            // get the latitude and longitude somehow???
+
+            Xamarin.Essentials.Location location =  await Xamarin.Essentials.Geolocation.GetLocationAsync();
+
+            latitude = location.Latitude;
+            longitude = location.Longitude;
+
+            LatitudeLabel.Text = latitude.ToString();
+            LongitudeLabel.Text = longitude.ToString();
+        }
+    }
+}
